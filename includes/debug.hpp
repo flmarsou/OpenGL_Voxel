cf0b940c7bc7fff60008bec0cb68eb8f2adfdd49
flmarsou <marsouinflorent12@gmail.com>
2025-07-09 10:05:49 +0200

    Added debug log function

---
+++ includes/debug.hpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+
+# define GREEN	"\e[32m"
+# define BLUE	"\e[36m"
+# define PURPLE	"\e[35m"
+# define RED	"\e[31m"
+# define RESET	"\e[0m"
+
+enum	debugType_e
+{
+	SUCCESS,
+	INFO,
+	WARNING,
+	ERROR,
+};
+
+void	debug(const std::string message, debugType_e type)
+{
+	std::time_t	time = std::time(nullptr);
+	std::tm		*localTime = std::localtime(&time);
+
+	switch (type)
+	{
+		case (SUCCESS): std::cout << GREEN; break ;
+		case (INFO): std::cout << BLUE; break ;
+		case (WARNING): std::cout << PURPLE; break ;
+		case (ERROR): std::cout << RED; break ;
+		default: break ;
+	}
+
+	std::cout << "[" << std::put_time(localTime, "%H:%M:%S") << "] " << message << RESET << std::endl;
+}
