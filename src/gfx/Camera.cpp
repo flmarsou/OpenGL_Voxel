910f60a7b1db6babfbb0338048bf04c1cdf8e7c1
flmarsou <marsouinflorent12@gmail.com>
2025-07-14 12:47:51 +0200

    Added Camera class

---
+++ src/gfx/Camera.cpp
@@ -0,0 +1,76 @@
+#include "Camera.hpp"
+
+void	Camera::Init(const u32 width, const u32 height, const glm::vec3 pos)
+{
+	this->width = width;
+	this->height = height;
+	this->Position = pos;
+}
+
+void	Camera::Matrix(float nearPlane, float farPlane, u32 shader, const char *uniform)
+{
+	glm::mat4	view = glm::lookAt(
+		this->Position,
+		this->Position + this->Direction,
+		this->Angle
+	);
+
+	glm::mat4	proj = glm::perspective(
+		glm::radians(CAMERA_FOV),
+		this->width / this->height,
+		nearPlane,
+		farPlane
+	);
+
+	glUniformMatrix4fv(glGetUniformLocation(shader, uniform), 1, GL_FALSE, glm::value_ptr(proj * view));
+}
+
+void	Camera::Input(GLFWwindow *window)
+{
+	// --- Positional Movements ---
+	// Foward
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		this->Position += CAMERA_SPEED * this->Direction;
+
+	// Left
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		this->Position += CAMERA_SPEED * -glm::normalize(glm::cross(this->Direction, this->Angle));
+
+	// Backward
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		this->Position += CAMERA_SPEED * -this->Direction;
+
+	// Left
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		this->Position += CAMERA_SPEED * glm::normalize(glm::cross(this->Direction, this->Angle));
+
+	// Up
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+		this->Position += CAMERA_SPEED * this->Angle;
+
+	// Down
+	if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
+		this->Position -= CAMERA_SPEED * this->Angle;
+
+	// --- Directional Movement ---
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+	{
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+
+	double	mouseX;
+	double	mouseY;
+	glfwGetCursorPos(window, &mouseX, &mouseY);
+
+	float	rotX = CAMERA_SENSITIVITY * (float)(mouseY - this->height / 2);
+	float	rotY = CAMERA_SENSITIVITY * (float)(mouseX - this->width / 2);
+
+	glm::vec3	newDirection = glm::rotate(this->Direction, glm::radians(-rotX), glm::normalize(glm::cross(this->Direction, this->Angle)));
+
+	if (!(glm::angle(newDirection, this->Angle) <= glm::radians(5.0f)) || (glm::angle(newDirection, -this->Angle) <= glm::radians(5.0f)))
+		this->Direction = newDirection;
+
+	this->Direction = glm::rotate(this->Direction, glm::radians(-rotY), this->Angle);
+
+	glfwSetCursorPos(window, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
+	}
+}
---
+++ src/gfx/Camera.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+# include "types.hpp"
+# include "options.hpp"
+
+# include "VoxelShader.hpp"
+
+# define GLM_ENABLE_EXPERIMENTAL
+# include <GLFW/glfw3.h>
+# include <GLM/glm.hpp>
+# include <GLM/gtc/matrix_transform.hpp>
+# include <GLM/gtc/type_ptr.hpp>
+# include <GLM/gtx/rotate_vector.hpp>
+# include <GLM/gtx/vector_angle.hpp>
+
+class	Camera
+{
+	public:
+		void	Init(const u32 width, const u32 height, const glm::vec3 pos);
+
+		void	Matrix(float nearPlane, float farPlane, u32 shader, const char *uniform);
+		void	Input(GLFWwindow *window);
+
+		glm::vec3	Position;
+		glm::vec3	Direction = glm::vec3(0.0f, 0.0f, -1.0f);
+		glm::vec3	Angle = glm::vec3(0.0f, 1.0f, 0.0f);
+
+		float		width;
+		float		height;
+};
