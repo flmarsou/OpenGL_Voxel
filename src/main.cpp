0c5477b0ae4edf550b4b863b4661e36565f9317a
flmarsou <marsouinflorent12@gmail.com>
2025-07-14 12:46:05 +0200

    Added main

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include "options.hpp"
+#include "Window.hpp"
+#include "Renderer.hpp"
+
+i32	main()
+{
+	Window		window;
+	Renderer	renderer;
+
+	try
+	{
+		window.Init(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
+		renderer.Init();
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << ERROR << e.what() << RESET << std::endl;
+		return (1);
+	}
+
+	while (!window.ShouldClose())
+	{
+		window.Clear();
+
+		renderer.Render(window.win);
+
+		window.SwapBuffers();
+		window.PollEvents();
+
+		// renderer.PrintStatistics();
+	}
+
+	window.Cleanup();
+	renderer.Cleanup();
+
+	return (0);
+}
