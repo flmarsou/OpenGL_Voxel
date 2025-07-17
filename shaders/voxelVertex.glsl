bf2cc9ac582d48f8f740dc9280fe09015a701081
flmarsou <marsouinflorent12@gmail.com>
2025-07-17 19:41:05 +0200

    WIP

--- shaders/voxelVertex.glsl
+++ shaders/voxelVertex.glsl
@@ -1,5 +1,6 @@
 #version 460 core
 
+// ! Send Voxel/Vertex Pack instead, so GPU can Unpack it
 layout (location = 0) in vec3	aPos;
 layout (location = 1) in vec2	aTex;
 layout (location = 2) in float	blockID;
