d53a240bdfb81544c5da088975fbba56a0911062
flmarsou <marsouinflorent12@gmail.com>
2025-07-21 22:20:42 +0200

    Made better world generation

--- src/world/ChunkGeneration.cpp
+++ src/world/ChunkGeneration.cpp
@@ -4,11 +4,17 @@
 
 void	Chunk::GenerateVoxels()
 {
-	FastNoiseLite	noise;
+	FastNoiseLite	baseNoise;
 
-	noise.SetNoiseType(FastNoiseLite::NoiseType_Perlin);
-	noise.SetFrequency(0.01f);
-	noise.SetSeed(42);
+	baseNoise.SetNoiseType(FastNoiseLite::NoiseType_Perlin);
+	baseNoise.SetFrequency(0.01f);
+	baseNoise.SetSeed(SEED);
+
+	FastNoiseLite	detailNoise;
+
+	detailNoise.SetNoiseType(FastNoiseLite::NoiseType_Perlin);
+	detailNoise.SetFrequency(0.05f);
+	detailNoise.SetSeed(SEED);
 
 	for (u8 x = 0; x < CHUNK_WIDTH; x++)
 	for (u8 z = 0; z < CHUNK_WIDTH; z++)
@@ -16,8 +22,12 @@ void	Chunk::GenerateVoxels()
 		i32	worldX = this->_chunkX * CHUNK_WIDTH + x;
 		i32	worldZ = this->_chunkZ * CHUNK_WIDTH + z;
 
-		float	noiseValue = noise.GetNoise(static_cast<float>(worldX), static_cast<float>(worldZ));
-		i32		height = static_cast<i32>((noiseValue + 1) * 0.5f * (CHUNK_HEIGHT - 1));
+		float	baseValue = baseNoise.GetNoise(static_cast<float>(worldX), static_cast<float>(worldZ));
+		float	detailValue = detailNoise.GetNoise(static_cast<float>(worldX), static_cast<float>(worldZ));
+
+		float	fusedValues = baseValue * 0.7f + detailValue * 0.3f;
+
+		i32		height = static_cast<i32>((fusedValues + 1) * 0.5f * (CHUNK_HEIGHT - 1));
 
 		for (u8 y = 0; y < CHUNK_HEIGHT; y++)
 		{
